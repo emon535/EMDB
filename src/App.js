3b5320c5d90119bcd9fbba318b4f1ed261a51461
Foysal Ahmed Emon <foysal.emon@cefalo.com>
2021-10-27 16:02:49 +0600

    Added Tailwind Css and Showed the movie list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import { useGetMovieByPageNumberQuery } from "./services/movieApiService";
 
+const MovieCard =(props)=>{
+  return <div>
+    <li >{props.data.title}</li>
+  </div>
+}
+
+
 function App() {
 
-  const {data,error, isLoading, isSuccess, isError} = useGetMovieByPageNumberQuery({listId:"1", pageNo:"1"});
+  const {data,error, isLoading, isSuccess, isError} = useGetMovieByPageNumberQuery({listId:"3", pageNo:"2"});
   console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ error", error)
   console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ isLoading", isLoading)
   console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ data", data)
@@ -13,7 +20,12 @@ function App() {
       {isError && <h4>Something went wrong</h4>}
       {isSuccess && <h4>Movie Database is parsed successfully</h4>}
       <div >
-        
+      
+        {isSuccess && data && data.results.map(movie=>{
+          return<div class="w-full lg:w">
+              <MovieCard data ={movie}/>
+          </div>
+        })}
       </div>
     </div>
   );
